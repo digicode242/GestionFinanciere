bbd3376fc64b6ba1594fdf9e67691ff623472e84
MOULOUNDOU <mardochekimbassa9@gmail.com>
2026-04-26 23:56:50 +0000

    Footer Nav+Contact in row on mobile, hero content centered, AOS scroll animations restored

--- script.js
+++ script.js
@@ -7,6 +7,17 @@ if (window.lucide) {
     lucide.createIcons();
 }
 
+// AOS : animations au scroll
+if (window.AOS) {
+    AOS.init({
+        duration: 700,
+        easing: 'ease-out-cubic',
+        once: true,
+        offset: 60,
+        delay: 0
+    });
+}
+
 // Swiper : carrousel des aperçus
 if (window.Swiper) {
     new Swiper('.showcase-swiper', {
